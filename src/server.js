1a7cff9f53ce64e76b110a223019665103e31efb
Tbrainyz <gbadebo.wale004@gmail.com>
2026-05-27 10:48:53 +0100

    fix

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
-  'https://real-custom-back.onrender.com',
+  'https://real-custom-back.vercel.app/',
 ];
 
 app.use(
