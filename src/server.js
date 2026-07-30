7e4b9aa75f1c743e5fb0196d38f7bab7cde7a925
Tbrainyz <gbadebo.wale004@gmail.com>
2026-07-30 10:02:30 +0100

    sub pend

--- src/server.js
+++ src/server.js
@@ -72,6 +72,7 @@ app.use('/dashboard', require('./routes/dashboard'));
 app.use('/payments', require('./routes/payments'));
 app.use('/users', require('./routes/users'));
 app.use('/debug', require('./routes/debug'));
+app.use('/subscription', require('./routes/subscription'));
 
 // 404 Handler
 app.use((req, res) => {
